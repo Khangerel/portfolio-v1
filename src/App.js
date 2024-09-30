b0141aef9e19702a93c66d06466c5e4919f74aa9
Khangerel <49606526+Khangerel@users.noreply.github.com>
2024-09-30 18:04:05 -0500

    first commit of v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import styled from "styled-components";
+import { BsLinkedin, BsGithub, BsMailbox2 } from "react-icons/bs";
+
+const H5 = styled.h4`
+    -webkit-font-smoothing: antialiased;
+    font-smoothing: antialiased;
+    font-size: calc(48px + 2vmin);
+    margin: 20px;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const linkedInUrl = "https://www.linkedin.com/in/khangerel-ganzorig/";
+    const githubUrl = "https://github.com/Khangerel";
+    const resumeUrl =
+        "https://resume-ganzokhan.s3.us-east-2.amazonaws.com/Khangerel_Ganzorig_Resume.pdf";
+    const email = "ganzokhan@gmail.com";
+    return (
+        <div className="App">
+            <header className="App-header">
+                <H5>Khangerel Ganzorig</H5>
+                <p style={{ margin: 5, color: "grey" }}>
+                    Software Engineer | Full Stack Developer
+                </p>
+                <p style={{ margin: 5, color: "grey" }}>
+                    Please feel free to contact me!
+                </p>
+                <button
+                    className="App-button"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        const link = document.createElement("a");
+                        link.href = `${resumeUrl}`;
+                        link.target = "_self";
+                        link.style.display = "none";
+                        document.body.appendChild(link);
+                        setTimeout(() => {
+                          link.click();
+                          document.body.removeChild(link);
+                        }, 0);
+                    }}
+                >
+                    Resume
+                </button>
+                <Row
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        flexDirection: "row",
+                    }}
+                >
+                    <Col key={1} xs={6} md={4} lg={3}>
+                        <button
+                            className="Social-button"
+                            onClick={() => window.open(linkedInUrl, "_blank")}
+                        >
+                            <BsLinkedin size={20} />
+                        </button>
+                    </Col>
+                    <Col key={2} xs={6} md={4} lg={3}>
+                        <button
+                            className="Social-button"
+                            onClick={() => window.open(githubUrl, "_blank")}
+                        >
+                            <BsGithub size={20} />
+                        </button>
+                    </Col>
+                    <Col key={3} xs={6} md={4} lg={3}>
+                        <button
+                            className="Social-button"
+                            onClick={() => window.open(`mailto:${email}`)}
+                        >
+                            <BsMailbox2 size={20} />
+                        </button>
+                    </Col>
+                </Row>
+            </header>
+        </div>
+    );
 }
 
 export default App;
